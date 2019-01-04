dd67887c68eabfd81c063c692534f00774091d1d
nikecan <1659717703@qq.com>
2019-01-04 13:49:20 +0800

    后台开发完成

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,25 @@ var mongo = require('../mongo');
 router.get('/api/get/list', function (req, res, next) {
   var params = req.query,
     page = params.page,
-    page_size = params.page_size *1,
-    type = params.type*1;
-  console.log(params)
+    page_size = params.page_size * 1,
+    type = params.type * 1;
   mongo.connect(function (err, cols, db) {
     if (err) {
       res.json({ code: 0, msg: err });
     } else {
-      var sizeNum = (page - 1) * page_size;
-      cols.find({ type: type }).skip(sizeNum).limit(page_size).toArray(function (error, results) {
-        db.close();
-        if (error) {
-          res.json({ code: 0, msg: errors });
-        } else {
-          res.json({ code: 1, data: results });
-        }
+      cols.find({ type: type }).count(function (error, results) {
+        var total = Math.ceil(results / page_size);
+        
+        var sizeNum = (page - 1) * page_size;
+        cols.find({ type: type }).skip(sizeNum).limit(page_size).toArray(function (error, results) {
+          db.close();
+          if (error) {
+            res.json({ code: 0, msg: error });
+          } else {
+            console.log(results)
+            res.json({ code: 1, data: results,total:total});
+          }
+        })
       })
     }
   })
